52779ae609d747af81bb254ec2cdb696171beae4
Angus-Leung <angusleung@Anguss-MacBook-Pro.local>
2015-04-04 17:02:09 -0400

    templating queue.h (unfinished)

--- Queue.h
+++ Queue.h
@@ -2,7 +2,9 @@
 #define QUEUE_H
 
 #include "Pirate.h"
+#include <cstdlib>
 
+template <class T>
 class Queue
 {
     class Node 
@@ -16,20 +18,71 @@ class Queue
     
     public:
         Queue();
-        Queue(Queue&);
+        Queue(Queue<T>&);
         ~Queue();
         void pop();
-        Pirate* front();
-        int getPirateSpace(int);
-        Pirate* find(int);
+        T* front();
+        int getPirateSpace(int pID);
+        T* find(int);
         void deleteData();
-        Queue& operator+=(Pirate*);
-        Queue& operator-=(Pirate*);
-        Pirate* operator[](int);
+        Queue<T>& operator+=(T*);
+        Queue<T>& operator-=(T*);
+        T* operator[](int index);
         bool operator!();
     
     private:
         Node* head;
 };
+
+template <class T>
+Queue<T>::Queue() : head (0){ }
+
+template <class T>
+Queue<T>::Queue(Queue<T>){
+    head = 0;
+    Node *origCurrNode;
+    
+    origCurrNode = origQueue.head;
+    
+    while (origCurrNode != 0) {
+        *this += origCurrNode->data;
+        origCurrNode = origCurrNode->next;
+    }
+}
+
+template <class T>
+Queue<T>::~Queue(){ }
+
+//to do
+template <class T>
+void Queue<T>::pop(){ }
+
+//to do
+template <class T>
+T* Queue<T>::front(){ }
+
+//to do
+template <class T>
+int Queue<T>::getPirateSpace(int pID){ }
+
+//to do
+template <class T>
+T* Queue<T>::find(int){ }
+
+//to do
+template <class T>
+void Queue<T>::deleteData(){ }
+
+//to do
+template <class T>
+Queue<T>& Queue<T>::operator+=(T*){ }
+
+//to do
+template <class T>
+Queue<T>& Queue<T>::operator-=(T*){ }
+
+//to do
+template <class T>
+bool Queue<T>::operator!(){ }
 
 #endif
